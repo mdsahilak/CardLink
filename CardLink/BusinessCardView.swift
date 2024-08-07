26447b8e415f9764f21c0ea33418297198ac0adb
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-08-08 00:42:31 +0100

    Add App Icon

--- CardLink/BusinessCardView.swift
+++ CardLink/BusinessCardView.swift
@@ -12,21 +12,23 @@ struct BusinessCardView: View {
     
     var body: some View {
         ZStack {
-            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00)
+            Color.gray
                 .edgesIgnoringSafeArea(.all)
             
             VStack {
-                Image("memoji_dark")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 100, height: 100, alignment: .center)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
-                
-                Text("Md Sahil Ak")
-                    .font(.custom("Pacifico-Regular", size: 40))
-                    .bold()
-                    .foregroundColor(.white)
+                HStack {
+                    Image("memoji_dark")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 100, height: 100, alignment: .center)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                    
+                    Text("Md Sahil Ak")
+                        .font(.headline)
+                        .bold()
+                        .foregroundColor(.white)
+                }
                 
                 Text("iOS Developer")
                     .font(.system(size: 20))
@@ -37,11 +39,12 @@ struct BusinessCardView: View {
                 InfoView(infoText: "mdsahilak@gmail.com", iconName: "envelope.fill", iconColor: .green)
                 InfoView(infoText: "+91 8747084769", iconName: "phone.fill", iconColor: .green)
             }
+            .padding()
         }
         .padding()
         .background {
-            RoundedRectangle(cornerRadius: 13)
-                .foregroundStyle(Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00))
+            RoundedRectangle(cornerRadius: 7)
+                .foregroundStyle(Color.gray)
         }
         .frame(maxHeight: 250)
         .padding()

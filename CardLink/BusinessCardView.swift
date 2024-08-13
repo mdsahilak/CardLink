afe3eaae8eb093346fdddc9473def4ca3897cbd4
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-08-13 20:37:00 +0100

    Build basic navigation infra

--- CardLink/BusinessCardView.swift
+++ CardLink/BusinessCardView.swift
@@ -11,47 +11,44 @@ struct BusinessCardView: View {
     let card: BusinessCard
     
     var body: some View {
-        ZStack {
-            Color.gray
-                .edgesIgnoringSafeArea(.all)
+        HStack {
+            IconView
+            Divider()
             
-            VStack {
-                HStack {
-                    Image("memoji_dark")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 100, height: 100, alignment: .center)
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.white, lineWidth: 5))
-                    
-                    Text("Md Sahil Ak")
-                        .font(.appSubheadline)
-                        .bold()
-                        .foregroundColor(.white)
-                }
+            InfoView
+            Spacer()
+        }
+    }
+    
+    private var IconView: some View {
+        Image("memoji_light")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 70, height: 70, alignment: .center)
+            .clipShape(Circle())
+            .font(.appTitle1)
+    }
+    
+    private var InfoView: some View {
+        VStack(alignment: .leading, spacing: 7) {
+            VStack(alignment: .leading, spacing: 3) {
+                Text("Chief Executive Officer")
+                    .font(.appFootnote)
                 
-                Text("iOS Developer")
-                    .font(.system(size: 20))
-                    .foregroundColor(.white)
-                
-                Divider()
-                
-                InfoView(infoText: "mdsahilak@gmail.com", iconName: "envelope.fill", iconColor: .green)
-                InfoView(infoText: "+91 8747084769", iconName: "phone.fill", iconColor: .green)
+                Text("John Appleseed")
+                    .font(.appTitle2)
             }
-            .padding()
+            
+            Divider()
+            
+            Text("Apple, Inc.")
+                .font(.appSubheadline)
         }
-        .padding()
-        .background {
-            RoundedRectangle(cornerRadius: 7)
-                .foregroundStyle(Color.gray)
-        }
-        .frame(maxHeight: 250)
-        .padding()
-        
     }
 }
 
 #Preview {
     BusinessCardView(card: .mock)
+        .frame(maxHeight: 100)
+        .padding()
 }
--- CardLink/HomeView.swift
+++ CardLink/HomeView.swift
@@ -8,55 +8,61 @@
 import SwiftUI
 
 struct HomeView: View {
+    @State private var searchText = ""
+    
     var body: some View {
-        ZStack {
-            Color(red: 0.09, green: 0.63, blue: 0.52, opacity: 1.00)
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack {
-                Image("memoji_dark")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 200, height: 200, alignment: .center)
-                    .clipShape(Circle())
-                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
-                Text("Md Sahil Ak")
-                    .font(.custom("Pacifico-Regular", size: 40))
-                    .bold()
-                    .foregroundColor(.white)
-                Text("iOS Developer")
-                    .font(.system(size: 20))
-                    .foregroundColor(.white)
+        NavigationStack {
+            Form {
+                Section {
+                    searchBar
+                }
                 
-                Divider()
+                Section {
+                    BusinessCardView(card: .mock)
+                }
                 
-                InfoView(infoText: "mdsahilak@gmail.com", iconName: "envelope.fill", iconColor: .green)
-                InfoView(infoText: "+91 8747084769", iconName: "phone.fill", iconColor: .green)
+                Section {
+                    BusinessCardView(card: .mock)
+                }
+                
+                Section {
+                    BusinessCardView(card: .mock)
+                }
             }
-            
+            .foregroundStyle(.primaryText)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("CardLink")
+                        .font(.appTitle3)
+                        .foregroundColor(.primaryText)
+                }
+            }
         }
         
     }
-}
-
-struct InfoView: View {
     
-    var infoText: String
-    var iconName: String
-    var iconColor: Color
-    
-    var body: some View {
-        RoundedRectangle(cornerRadius: 1000)
-            .fill(Color.white)
-            .frame(height: 50)
-            .overlay(HStack {
-                Image(systemName: iconName)
-                    .foregroundColor(iconColor)
-                Text(infoText)
-            })
-            .padding(EdgeInsets(top: 16, leading: 12, bottom: 0, trailing: 12))
+    private var searchBar: some View {
+        HStack {
+            TextField("Search", text: $searchText)
+                .textFieldStyle(.plain)
+            
+            if !searchText.isEmpty {
+                Image(systemName: "xmark.circle")
+                    .font(.appBody)
+                    .contentShape(Rectangle())
+                    .frame(width: 44, alignment: .trailing)
+                    .onTapGesture {
+                        withAnimation(.default) {
+                            searchText = ""
+                        }
+                    }
+            }
+        }
+        .font(.body)
+        .tint(.primaryText)
+        .accessibilityAddTraits(.isSearchField)
     }
-    
 }
 
 #Preview {

d559a27261204a11208d4af98085af7bc1b18af3
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-08-28 19:43:13 +0100

    changes to the user flow for connecting peers

--- CardLink/NearbyExchange/NearbyExchangeView.swift
+++ CardLink/NearbyExchange/NearbyExchangeView.swift
@@ -11,17 +11,27 @@ struct NearbyExchangeView: View {
     @StateObject var model = NearbyExchangeViewModel()
 
     var body: some View {
-        NavigationStack {
+        NavigationStack(path: $model.joinedPeers) {
             List(model.peers) { peer in
-                HStack {
-                    Image(systemName: "iphone.gen1")
-                        .imageScale(.large)
-                        .foregroundColor(.accentColor)
+                Button(action: {
+                    model.selectedPeer = peer
+                }, label: {
+                    HStack {
+                        Image(systemName: "iphone.gen1")
+                            .imageScale(.large)
+                            .foregroundColor(.accentColor)
 
-                    Text(peer.peerId.displayName)
-                        .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(peer.peerId.displayName)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                    }
+                    .padding(.vertical, 5)
+                })
+                
+                Divider()
+                
+                ForEach(model.messages, id: \.self) { message in
+                    Text(message)
                 }
-                .padding(.vertical, 5)
             }
             .onAppear {
                 model.startBrowsing()
@@ -29,10 +39,39 @@ struct NearbyExchangeView: View {
             .onDisappear {
                 model.finishBrowsing()
             }
+            .alert(item: $model.permissionRequest, content: { request in
+                Alert(
+                    title: Text("Would you like to exchange cards with \(request.peerId.displayName)"),
+                    primaryButton: .default(Text("Yes"), action: {
+                        request.onRequest(true)
+                        model.show(peerId: request.peerId)
+                        print("Success")
+                    }),
+                    secondaryButton: .cancel(Text("No"), action: {
+                        request.onRequest(false)
+                    })
+                )
+            })
+            .navigationDestination(for: NearbyExchangeViewModel.PeerDevice.self, destination: { peer in
+                VStack {
+                    Text(peer.peerId.displayName.description)
+                    Divider()
+                    List {
+                        ForEach(model.messages, id: \.self) { message in
+                            Text(message)
+                        }
+                    }
+                }
+            })
             .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Toggle("Press to be discoverable", isOn: $model.isAdvertised)
-                        .toggleStyle(.switch)
+                ToolbarItem(placement: .principal) {
+                    Text("Searching for Peers")
+                }
+                
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Send Hello") {
+                        model.send(string: "Hello \(UUID().uuidString)")
+                    }
                 }
             }
         }
--- CardLink/NearbyExchange/NearbyExchangeViewModel.swift
+++ CardLink/NearbyExchange/NearbyExchangeViewModel.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import MultipeerConnectivity
+import Combine
 
 @MainActor
 final class NearbyExchangeViewModel: NSObject, ObservableObject {
@@ -15,13 +16,31 @@ final class NearbyExchangeViewModel: NSObject, ObservableObject {
     private let serviceType = "nearby-devices"
     private let browser: MCNearbyServiceBrowser
     
-    @Published var isAdvertised: Bool = false {
+    @Published var peers: [PeerDevice] = []
+    
+    @Published var permissionRequest: PermitionRequest?
+    
+    @Published var selectedPeer: PeerDevice? {
         didSet {
-            isAdvertised ? advertiser.startAdvertisingPeer() : advertiser.stopAdvertisingPeer()
+            connect()
         }
     }
     
-    @Published var peers: [PeerDevice] = []
+    @Published var joinedPeers: [PeerDevice] = []
+    
+    @Published var messages: [String] = []
+    let messagePublisher = PassthroughSubject<String, Never>()
+    var subscriptions = Set<AnyCancellable>()
+    
+    func send(string: String) {
+        if let data = string.data(using: .utf8), let peerID = joinedPeers.last?.peerId {
+            try? session.send(data, toPeers: [peerID], with: .reliable)
+            
+            messagePublisher.send(string)
+        } else {
+            print("Error sendind data")
+        }
+    }
     
     override init() {
         let peer = MCPeerID(displayName: UIDevice.current.name)
@@ -36,20 +55,57 @@ final class NearbyExchangeViewModel: NSObject, ObservableObject {
         browser = MCNearbyServiceBrowser(peer: peer, serviceType: serviceType)
         
         super.init()
+        
         browser.delegate = self
+        advertiser.delegate = self
+        session.delegate = self
+        
+        messagePublisher
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] in
+                self?.messages.append($0)
+            }
+            .store(in: &subscriptions)
     }
     
     func startBrowsing() {
-            browser.startBrowsingForPeers()
+        advertiser.startAdvertisingPeer()
+        browser.startBrowsingForPeers()
+    }
+        
+    func finishBrowsing() {
+        advertiser.stopAdvertisingPeer()
+        browser.stopBrowsingForPeers()
+    }
+    
+    func show(peerId: MCPeerID) {
+        guard let first = peers.first(where: { $0.peerId == peerId }) else {
+            return
         }
         
-        func finishBrowsing() {
-            browser.stopBrowsingForPeers()
+        joinedPeers.append(first)
+    }
+    
+    private func connect() {
+        guard let selectedPeer else {
+            return
         }
+        
+        if session.connectedPeers.contains(selectedPeer.peerId) {
+            joinedPeers.append(selectedPeer)
+        } else {
+            browser.invitePeer(selectedPeer.peerId, to: session, withContext: nil, timeout: 60)
+        }
+    }
 }
 
 
 extension NearbyExchangeViewModel: MCNearbyServiceBrowserDelegate {
+    struct PeerDevice: Identifiable, Hashable {
+        let id = UUID()
+        let peerId: MCPeerID
+    }
+    
     func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
         peers.append(PeerDevice(peerId: peerID))
     }
@@ -57,9 +113,53 @@ extension NearbyExchangeViewModel: MCNearbyServiceBrowserDelegate {
     func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
         peers.removeAll(where: { $0.peerId == peerID })
     }
-    
-    struct PeerDevice: Identifiable, Hashable {
+}
+
+
+extension NearbyExchangeViewModel: MCNearbyServiceAdvertiserDelegate {
+    struct PermitionRequest: Identifiable {
         let id = UUID()
         let peerId: MCPeerID
+        let onRequest: (Bool) -> Void
+    }
+    
+    func advertiser(
+        _ advertiser: MCNearbyServiceAdvertiser,
+        didReceiveInvitationFromPeer peerID: MCPeerID,
+        withContext context: Data?,
+        invitationHandler: @escaping (Bool, MCSession?) -> Void
+    ) {
+        permissionRequest = PermitionRequest(
+            peerId: peerID,
+            onRequest: { [weak self] permission in
+                invitationHandler(permission, permission ? self?.session : nil)
+            }
+        )
+    }
+}
+
+extension NearbyExchangeViewModel: MCSessionDelegate {
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        return
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        return
+    }
+    
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        return
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        return
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        guard let last = joinedPeers.last, last.peerId == peerID, let message = String(data: data, encoding: .utf8) else {
+            return
+        }
+
+        messagePublisher.send(message)
     }
 }

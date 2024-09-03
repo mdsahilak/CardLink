eebc8fa1a245402e7464739121df9576744bcb64
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-09-03 16:02:47 +0100

    code changes

--- CardLink/DocumentCameraView.swift
+++ CardLink/DocumentCameraView.swift
@@ -26,10 +26,12 @@ struct DocumentCameraView: UIViewControllerRepresentable {
             
             let request = VNRecognizeTextRequest { (request, error) in
                 guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+                
                 for observation in observations {
                     guard let bestCandidate = observation.topCandidates(1).first else { continue }
                     scannedTexts.append(bestCandidate.string)
                 }
+                
                 self.parent.recognizedText = scannedTexts.joined(separator: "\n")
             }
 
@@ -49,16 +51,18 @@ struct DocumentCameraView: UIViewControllerRepresentable {
 
     @Binding var recognizedText: String
     @Environment(\.presentationMode) var presentationMode
-
+    
     func makeCoordinator() -> Coordinator {
         return Coordinator(parent: self)
     }
-
+    
     func makeUIViewController(context: Context) -> VNDocumentCameraViewController {
         let viewController = VNDocumentCameraViewController()
+        
         viewController.delegate = context.coordinator
+        
         return viewController
     }
 
-    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {}
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: Context) {  }
 }

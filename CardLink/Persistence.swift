c2081bdc6c5c7e86904d67b59b286e9f53090a69
Muhammed Sahil AK <mdsahilak.dev@gmail.com>
2024-09-12 04:10:59 +0100

    add automatic core data saving support

--- CardLink/Persistence.swift
+++ CardLink/Persistence.swift
@@ -32,12 +32,23 @@ struct PersistenceController {
         description?.setOption(true as NSNumber, forKey: remoteChangeKey)
         
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            
             if let error = error as NSError? {
                 fatalError("Unresolved error \(error), \(error.userInfo)")
-                
             }
         })
         
         container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    func saveContext () {
+        let context = container.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                print(error as NSError)
+            }
+        }
     }
 }
